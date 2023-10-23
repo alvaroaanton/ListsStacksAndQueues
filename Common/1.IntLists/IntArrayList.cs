78d42d2140cf8c6edee659e130bb5363e4f4f009
ALVARO ANTON GARRIDO <aanton028@ikasle.ehu.eus>
2023-10-23 16:11:05 +0200

    IntList y ArrayList hechas. Preguntar por metodo remove en ArrayList y en general

--- Common/1.IntLists/IntArrayList.cs
+++ Common/1.IntLists/IntArrayList.cs
@@ -10,6 +10,7 @@ namespace Common
         public IntArrayList(int n)
         {
             //TODO #1: initialize Values with an array of size n
+            Values = new int[n];
         }
         public string AsString()
         {
@@ -26,11 +27,24 @@ namespace Common
         public void Add(int value)
         {
             //TODO #2: add a new integer to the end of the list
+            int i=0;
+            while(i< Count())
+            {
+                i++
+            }
+            Values[i] = value;
         }
 
         public int Get(int index)
         {
             //TODO #3: return the element on the index-th position. YOU MUST USE GetNode(int). O if the position is out of bounds
+            for(int i=0; i < Count(); i++)
+            {
+                if (i == index)
+                {
+                    return Values[i];
+                }
+            }
             return 0;
         }
 
@@ -39,7 +53,12 @@ namespace Common
         public int Count()
         {
             //TODO #4: return the number of elements on the lis
-            return 0;
+            NumElements = 0;
+            for(int i=0; i< Values.Length ; i++)
+            {
+                NumElements++;
+            }
+            return NumElements;
         }
 
 
@@ -47,12 +66,24 @@ namespace Common
         public void Remove(int index)
         {
             //TODO #5: remove the element on the index-th position. Do nothing if position is out of bounds
+            if(index>=0 && index < Count() - 1)
+            {
+                for (int i = index; i < Count() - 1; i++)
+                {
+                    Values[i] = Values[i + 1];
+                }
+            }
+            Values[Count() - 1] = 0;
         }
 
 
         public void Clear()
         {
             //TODO #6: remove all the elements on the list
+            for (int i = 0; i < Count(); i++)
+            {
+                    Values[i] = null;
+            }
         }
     }
 }
--- Common/1.IntLists/IntList.cs
+++ Common/1.IntLists/IntList.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Xml.Linq;
 
 namespace Common
 {
@@ -38,11 +39,33 @@ namespace Common
         public void Add(int value)
         {
             //TODO #1: add a new integer to the end of the list
+            IntListNode node = First;
+            if(node != null)
+            {
+                while (node.Next != null)
+                {
+                    node = node.Next;
+
+                }
+                node.Next.Value = value;
+            }
         }
 
         private IntListNode GetNode(int index)
         {
             //TODO #2: Return the element in position 'index'
+            int currentPos = 0;
+            IntListNode node = First;
+            while (node != null)
+            {
+                if (index == currentPos)
+                {
+                    return node;
+                }
+                currentPos++;
+                node = node.Next;
+            }
+          
             return null;
         }
 
@@ -50,25 +73,69 @@ namespace Common
         public int Get(int index)
         {
             //TODO #3: return the element on the index-th position. YOU MUST USE GetNode(int). O if the position is out of bounds
+            int currentPos = 0;
+            IntListNode node = First;
+            
+            while (node != null)
+            {
+                if (index == currentPos)
+                {
+                    return node.Value;
+                }
+                currentPos++;
+                node = node.Next;
+            }
+
             return 0;
         }
 
-        
         public int Count()
         {
             //TODO #4: return the number of elements on the list
-            return 0;
+            int numElementos = 0;
+            IntListNode node = First;
+
+            while (node != null)
+            {
+                numElementos++;
+                node = node.Next;
+            }
+
+            return numElementos;
         }
         
         public void Remove(int index)
         {
             //TODO #5: remove the element on the index-th position. Do nothing if position is out of bounds
+            int currentPos = 0;
+            IntListNode node = First;
+            if(index == 0)
+            {
+                First = First.Next;
+                return;
+            }
+            while (node != null)
+            {
+                if (index == currentPos)
+                {
+                    GetNode(index - 1).Next = GetNode(index + 1);
+                }
+                currentPos++;
+                node = node.Next;
+            }
+
+            return;
         }
 
         
         public void Clear()
         {
             //TODO #6: remove all the elements on the list
+            IntListNode node = First;
+            if (node != null)
+            {
+                node.Next = null;
+            }
         }
     }
 }
